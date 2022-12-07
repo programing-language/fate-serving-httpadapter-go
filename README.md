1f29828de0903aa08e6cc8fa4562614fed178e81
cjx <1067446576@qq.com>
2022-12-07 09:41:48 +0800

    add: 请求分析

--- README.md
+++ README.md
@@ -14,3 +14,28 @@ docker logs -f adapter
 ```
 curl -v localhost:20000/feature -H 'Content-Type: application/json' -d '{}'
 ```
+# 请求分析测试
+```
+curl -v http://localhost:8080/feature?token=1234&page=1 \
+-H 'Content-Type: application/json' \
+-H 'app_key: 123456' \
+-H 'access_token: qwertyuiop' \
+-H 'expire_time: 1670395896' \
+-d '{
+    "head": {
+        "serviceId": "chenjinxin.cn#1995"
+    },
+    "body": {
+        "featureData": {
+            "x0": -2.123,
+            "x1": -1.456,
+            "x2": 0.789,
+            "x3": 1.101112,
+            "x4": 2.131415
+        },
+        "sendToRemoteFeatureData": {
+            "age": 18
+        }
+    }
+}'
+```
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"time"
 
@@ -16,8 +17,46 @@ func main() {
 	}
 }
 
+// 分析请求参数
+func AnalysisRequest(c *gin.Context) {
+	// 查询请求URL后面拼接的参数
+	token := c.Query("token")
+	// 查询请求URL后面的参数，如果没有填写默认值
+	page := c.DefaultQuery("page", "1")
+	size := c.DefaultQuery("size", "10")
+	// 输出 URL 后面拼接的参数
+	fmt.Printf("URL参数 >>>>>> token: %s; page: %s; size: %s", token, page, size)
+	fmt.Println()
+
+	// 从表单中查询参数
+	name := c.PostForm("name")
+	//从取得URL中参数，此处URL中没有name字段
+	name = c.Param("name")
+	// 从表单中查询参数,，如果没有就获取默认值
+	message := c.DefaultPostForm("message", "default")
+	fmt.Printf("表单参数 >>>>>> name: %s; message: %s;", name, message)
+	fmt.Println()
+
+	// 获取header指定字段
+	appKey := c.Request.Header.Get("app_key")
+	accessToken := c.Request.Header.Get("access_token")
+	expireTime := c.Request.Header.Get("expire_time")
+	fmt.Printf("header >>>>>> app_key: %s; access_token: %s; expire_time: %s;", appKey, accessToken, expireTime)
+	fmt.Println()
+	for k, v := range c.Request.Header {
+		fmt.Println(k, v)
+	}
+	fmt.Println()
+
+	// 获取 Body 值
+	bodyBytes, _ := ioutil.ReadAll(c.Request.Body)
+	fmt.Printf("Body >>>>>> bodyBytes:%s", bodyBytes)
+}
+
 // curl -v localhost:8080/feature
 func FeatureHandler(c *gin.Context) {
+	AnalysisRequest(c)
+
 	x_data := map[string]interface{}{}
 	rand.Seed(time.Now().UnixNano())
 	floats_list := randFloats(-5.0, 5.0, 10)
